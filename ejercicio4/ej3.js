043500b288c54a7b3c8460522060bb3bd621bf6f
cristianmino <cristianeisasa@gmail.com>
2022-11-18 21:29:41 -0300

    Agregando ejercicios 4

---
+++ ejercicio4/ej3.js
@@ -0,0 +1,13 @@
+function suma(numero){
+    var num1=0;
+    if (numero>0){
+        for (var i = 1; i <= numero; i++)
+        {
+            num1 = num1+i;
+        }
+    }
+    return(num1);
+}
+console.log("La suma es: " + suma(4));
+console.log("La suma es: " + suma(10));
+console.log("La suma es: " + suma(15))
